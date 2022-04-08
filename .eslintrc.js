8ab2581ff90c5b45ad15300b7ed1e3573d6516fc
Nurliman Diara <iman.knitto@gmail.com>
2022-04-08 09:56:26 +0700

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
 };
