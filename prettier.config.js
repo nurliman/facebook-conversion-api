17ea420ec5daee0918facfc7d76960bb5ede9ef8
Nurliman Diara <iman.knitto@gmail.com>
2022-04-08 09:57:53 +0700

    init prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+};
+
+module.exports = prettierConfig;
