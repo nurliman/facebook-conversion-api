60724e9de3956b46f34a57be7cfa7c90d77249ef
nurliman <nurlimandiara@gmail.com>
2022-11-04 16:34:32 +0700

    bundle the whole things

--- tsup.config.js
+++ tsup.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "tsup";
-import { globbySync } from "globby";
 
 export default defineConfig({
-  entryPoints: globbySync(["src/**/*.+(js|ts)", "!src/types"]),
-  format: ["esm"],
+  entryPoints: ["src/index.ts"],
+  format: ["cjs", "esm"],
   target: "node10",
   treeshake: true,
-  bundle: false,
+  bundle: true,
   sourcemap: true,
   dts: true,
   clean: true,
